b6fe6eb199d8c2383a6c4a5ff46c7725c9c13a25
realib1 <ibeevpersie@gmail.com>
2024-02-10 22:38:09 +0000

    Almost Done

--- main.js
+++ main.js
@@ -31,7 +31,44 @@ $(document).ready(function() {
    
   });
 
-  
-$(document).ready(function(){
 
-})
+  // Counter
+  const counters = document.querySelectorAll('.counters');
+  let count = document.getElementById('count');
+  const increase = document.getElementById('increase');
+  const decrease = document.getElementById('decrease');
+
+  const count1 = document.getElementById('count1');
+  const increase1 = document.getElementById('increase1');
+  const decrease1 = document.getElementById('decrease1');
+
+  let value = 0;
+  let value1 = 0;
+
+  increase.addEventListener('click', () => {
+    value++;
+    count.textContent = value;
+  })
+
+  decrease.addEventListener('click', () => {
+    count.textContent = value;
+    if (value <= 0) {
+      value = 0;
+    } else {
+      value--;
+    }
+  })
+
+  increase1.addEventListener('click', () => {
+    value1++;
+    count1.textContent = value1;
+  })
+
+  decrease1.addEventListener('click', () => {
+    count1.textContent = value;
+    if (value1 <= 0) {
+      value1 = 0;
+    } else {
+      value1--;
+    }
+  })
